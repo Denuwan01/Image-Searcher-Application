c79f14c8350730e6b978688ff4283d53dfd552a7
Denuwan Nammuni <denuwannammuni@gmail.com>
2024-10-11 11:31:23 +0530

    modify

--- script.js
+++ script.js
@@ -17,17 +17,39 @@ async function searchImages() {
 
     const results = data.results
 
-    if (page == 1){
+    if (page == 1) {
         searchResults.innerHTML = ""
     }
 
-    results.map((result) =>{
+    results.map((result) => {
         const imageWrapper = document.createElement('div')
         imageWrapper.classList.add("search-result")
         const image = document.createElement('img')
         image.src = result.urls.small
-        
-        })
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
 
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        imageWrapper.appendChild(imageWrapper);
 
+    });
+
+    page++
+    if (page > 1) {
+        showMore.style.display = "block"
+    }
 }
+
+formE1.addEventListener("submit", (event) => {
+    event.preventDefault()
+    page = 1;
+    searchImages()
+})
+
+showMore.addEventListener("click", () => {
+    searchImages()
+})
